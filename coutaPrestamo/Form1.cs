77da63f9be6101326a5dbfdc6574902245c6f2be
yileini100 <153277863+yileini100@users.noreply.github.com>
2024-03-04 20:17:21 -0400

    Update Form1.cs

--- coutaPrestamo/Form1.cs
+++ coutaPrestamo/Form1.cs
@@ -56,6 +56,35 @@ namespace coutaPrestamo
 
         }
 
+        private void btn_Guardar_Click(object sender, EventArgs e)
+{
+    DialogResult resultado = dlg_Guardar.ShowDialog();
+
+    // Verificar si el usuario presionó el botón Guardar
+    if (resultado == DialogResult.OK)
+    {
+
+        string ruta_archivo = dlg_Guardar.FileName;
+        string crea_texto = ruta_archivo;
+
+
+        using (StreamWriter archivo = File.CreateText(ruta_archivo))
+        {
+            archivo.WriteLine(txt_Prestamo.Text); archivo.WriteLine(txt_Tasa.Text); archivo.WriteLine(txt_Periodo.Text); archivo.WriteLine(txt_Cuota.Text);
+            foreach (DataGridViewRow fila in dgv_data.Rows)
+            {
+                // Obtiene los valores de las celdas (supongamos que son 4 columnas)
+                string valorColumna1 = fila.Cells[0].Value?.ToString() ?? "";
+                string valorColumna2 = fila.Cells[1].Value?.ToString() ?? "";
+                string valorColumna3 = fila.Cells[2].Value?.ToString() ?? "";
+                string valorColumna4 = fila.Cells[3].Value?.ToString() ?? "";
+                string valorColumna5 = fila.Cells[4].Value?.ToString() ?? "";
+
+                // Escribe los valores en el archivo de texto
+                archivo.WriteLine($"{valorColumna1}, {valorColumna2}, {valorColumna3},{valorColumna4},{valorColumna5}");
+
+            }
+
         private void btn_Salir_Click(object sender, EventArgs e)
         {
             Close();
